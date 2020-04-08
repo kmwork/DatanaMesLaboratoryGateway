b5ffe4e5754e1fd4b6725d9cf33101ada446f9f1
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-08 19:36:36 +0300

    версия 1

--- src/main/java/ru/datana/steel/mes/db/CallDbServiceImpl.java
+++ src/main/java/ru/datana/steel/mes/db/CallDbServiceImpl.java
@@ -10,7 +10,6 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.sql.SQLException;
 
 
 @Service
